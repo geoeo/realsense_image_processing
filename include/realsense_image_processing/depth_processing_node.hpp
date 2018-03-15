8d2fa73b2a92541bfda93353bbcb602d146d6c26
Marc-Ubuntu <marc.dhaubenstock@gmail.com>
2018-03-15 19:10:08 +0100

    initial commit

---
+++ include/realsense_image_processing/depth_processing_node.hpp
@@ -0,0 +1,10 @@
+//
+// Created by marc on 15.03.18.
+//
+
+#ifndef PROJECT_DEPTH_PROCESSING_NODE_HPP
+#define PROJECT_DEPTH_PROCESSING_NODE_HPP
+
+#include <ros/ros.h>
+
+#endif //PROJECT_DEPTH_PROCESSING_NODE_HPP
---
+++ src/depth_processing_node.cpp
@@ -0,0 +1,11 @@
+//
+// Created by marc on 15.03.18.
+//
+#include "realsense_image_processing/depth_processing_node.hpp"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "depth_processing");
+    ros::spin();
+    return 0;
+}
