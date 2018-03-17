90989168748bd612c1def8df3a693d7cc67b624c
Marc-Ubuntu <marc.dhaubenstock@gmail.com>
2018-03-17 04:21:04 +0100

    started depth frame processing

--- include/realsense_image_processing/depth_processing_node.hpp
+++ include/realsense_image_processing/depth_processing_node.hpp
@@ -6,5 +6,15 @@
 #define PROJECT_DEPTH_PROCESSING_NODE_HPP
 
 #include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/fill_image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+cv_bridge::CvImageConstPtr cv_ptr;
+static const std::string OPENCV_WINDOW = "Image window";
 
 #endif //PROJECT_DEPTH_PROCESSING_NODE_HPP
--- src/depth_processing_node.cpp
+++ src/depth_processing_node.cpp
@@ -3,9 +3,85 @@
 //
 #include "realsense_image_processing/depth_processing_node.hpp"
 
+class ImageConverter
+{
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+public:
+ImageConverter()
+        : it_(nh_)
+{
+
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/realsense/camera/depth/image_raw", 1, &ImageConverter::depthCallback,this);
+
+
+    cv::namedWindow(OPENCV_WINDOW);
+}
+
+~ImageConverter()
+{
+    cv::destroyWindow(OPENCV_WINDOW);
+}
+
+void depthCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+
+    //ROS_INFO("cv_bridge exception: %s", msg.encoding.c_str());
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(msg,sensor_msgs::image_encodings::MONO16);
+        //cv_ptr = cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::MONO16);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv::Mat view = cv_ptr->image;
+    cv::imshow(OPENCV_WINDOW, view);
+
+}
+};
+
+void depthCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+
+
+    //ROS_INFO("cv_bridge exception: %s", msg.encoding.c_str());
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(msg,msg->encoding);
+        //cv_ptr = cv_bridge::toCvCopy(msg,msg.encoding);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv::Mat view = cv_ptr->image;
+    cv::imshow(OPENCV_WINDOW, view);
+
+}
+
+
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "depth_processing");
+    //ImageConverter ic;
+    cv::namedWindow(OPENCV_WINDOW);
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber sub = it.subscribe("/realsense/camera/depth/image_raw", 2, depthCallback);
+                                                   //ros::VoidPtr(),image_transport::TransportHints("raw"));
+
     ros::spin();
+    //ros::spinOnce();
+
     return 0;
 }
+
