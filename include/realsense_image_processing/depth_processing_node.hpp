41d74e4eb4efc497f77acc5706aa642840d46190
Marc-Ubuntu <marc.dhaubenstock@gmail.com>
2018-03-17 16:00:28 +0100

    publishing simpe twist commands

--- include/realsense_image_processing/depth_processing_node.hpp
+++ include/realsense_image_processing/depth_processing_node.hpp
@@ -9,7 +9,7 @@
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 #include <image_transport/image_transport.h>
-#include <sensor_msgs/fill_image.h>
+#include <geometry_msgs/Twist.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
--- src/depth_processing_node.cpp
+++ src/depth_processing_node.cpp
@@ -13,13 +13,15 @@ class ImageConverter
     image_transport::Subscriber depth_sub_;
     ros::Subscriber color_sub_;
 public:
+    ros::Publisher pub_twist_cmd_;
 ImageConverter()
         : it_(nh_)
 {
 
     // Subscrive to input video feed and publish output video feed
     color_sub_ = nh_.subscribe("/realsense/camera/color/image_raw", 1, &ImageConverter::imageCallback,this);
-    depth_sub_ = it_.subscribe("/realsense/camera/depth/image_raw", 2, &ImageConverter::depthCallback,this);
+    depth_sub_ = it_.subscribe("/realsense/camera/depth/image_raw", 1, &ImageConverter::depthCallback,this);
+    pub_twist_cmd_ = nh_.advertise<geometry_msgs::Twist> ("ackermann/cmd_vel", 1 );
     //ROS_INFO("topic: %s",image_sub_.getTopic().c_str());
     //ROS_INFO("transport: %s",image_sub_.getTransport().c_str());
 
@@ -81,14 +83,26 @@ void depthCallback(const sensor_msgs::ImageConstPtr& image)
 }
 };
 
+void publishCmd (ImageConverter& ic) {
+    //boost::interprocess::scoped_lock<boost::mutex> scoped_lock ( mutex_ );
+    geometry_msgs::Twist twist;
+    twist.linear.x = 1.0;
+    twist.angular.z = 0.2;
+    ic.pub_twist_cmd_.publish ( twist );
+}
 
+// TODO: Look into locks when publishing motion commands which depend images
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "depth_processing");
     ImageConverter ic;
 
-    ros::spin();
-
+    //ros::spin();
+    while ( ros::ok()) {
+        ros::spinOnce();
+        publishCmd(ic);
+        ros::Duration(0.01).sleep();
+    }
 
     return 0;
 }
