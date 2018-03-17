7fe7dd9443f9a330dc249b359328f09d838d1956
Marc-Ubuntu <marc.dhaubenstock@gmail.com>
2018-03-17 15:10:47 +0100

    display of image topics

--- include/realsense_image_processing/depth_processing_node.hpp
+++ include/realsense_image_processing/depth_processing_node.hpp
@@ -13,8 +13,9 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <boost/thread.hpp>
 
-cv_bridge::CvImageConstPtr cv_ptr;
-static const std::string OPENCV_WINDOW = "Image window";
+static const std::string OPENCV_WINDOW_COLOR = "Color Image window";
+static const std::string OPENCV_WINDOW_DEPTH = "Depth Image window";
 
 #endif //PROJECT_DEPTH_PROCESSING_NODE_HPP
--- src/depth_processing_node.cpp
+++ src/depth_processing_node.cpp
@@ -3,36 +3,44 @@
 //
 #include "realsense_image_processing/depth_processing_node.hpp"
 
+
+// https://github.com/ros-perception/image_pipeline/blob/indigo/image_view/src/nodes/image_view.cpp
+// http://wiki.ros.org/cv_bridge/Tutorials/UsingCvBridgeToConvertBetweenROSImagesAndOpenCVImages
 class ImageConverter
 {
     ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
+    image_transport::Subscriber depth_sub_;
+    ros::Subscriber color_sub_;
 public:
 ImageConverter()
         : it_(nh_)
 {
 
     // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/realsense/camera/depth/image_raw", 1, &ImageConverter::depthCallback,this);
+    color_sub_ = nh_.subscribe("/realsense/camera/color/image_raw", 1, &ImageConverter::imageCallback,this);
+    depth_sub_ = it_.subscribe("/realsense/camera/depth/image_raw", 2, &ImageConverter::depthCallback,this);
+    //ROS_INFO("topic: %s",image_sub_.getTopic().c_str());
+    //ROS_INFO("transport: %s",image_sub_.getTransport().c_str());
 
 
-    cv::namedWindow(OPENCV_WINDOW);
+    cv::namedWindow(OPENCV_WINDOW_COLOR);
+    cv::namedWindow(OPENCV_WINDOW_DEPTH);
 }
 
 ~ImageConverter()
 {
-    cv::destroyWindow(OPENCV_WINDOW);
+    cv::destroyWindow(OPENCV_WINDOW_COLOR);
+    cv::destroyWindow(OPENCV_WINDOW_DEPTH);
 }
 
-void depthCallback(const sensor_msgs::ImageConstPtr& msg)
-{
+void imageCallback(const sensor_msgs::ImageConstPtr &image) {
 
-    //ROS_INFO("cv_bridge exception: %s", msg.encoding.c_str());
+    cv_bridge::CvImageConstPtr cv_ptr;
+
     try
     {
-        cv_ptr = cv_bridge::toCvShare(msg,sensor_msgs::image_encodings::MONO16);
-        //cv_ptr = cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::MONO16);
+        cv_ptr = cv_bridge::toCvShare(image,sensor_msgs::image_encodings::RGB8);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -41,46 +49,46 @@ void depthCallback(const sensor_msgs::ImageConstPtr& msg)
     }
 
     cv::Mat view = cv_ptr->image;
-    cv::imshow(OPENCV_WINDOW, view);
+    if(!view.empty()){
+        const cv::Mat &image_ref = view;
+        cv::imshow(OPENCV_WINDOW_COLOR, image_ref);
+        cv::waitKey(1); // super important
+    }
+}
+
+void depthCallback(const sensor_msgs::ImageConstPtr& image)
+{
+
+    cv_bridge::CvImageConstPtr cv_ptr;
+
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(image,sensor_msgs::image_encodings::MONO16);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv::Mat view = cv_ptr->image;
+    if(!view.empty()){
+        const cv::Mat &image_ref = view;
+        cv::imshow(OPENCV_WINDOW_DEPTH, image_ref);
+        cv::waitKey(1); // super important
+    }
 
 }
 };
-
-void depthCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-
-
-    //ROS_INFO("cv_bridge exception: %s", msg.encoding.c_str());
-    try
-    {
-        cv_ptr = cv_bridge::toCvShare(msg,msg->encoding);
-        //cv_ptr = cv_bridge::toCvCopy(msg,msg.encoding);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
-
-    cv::Mat view = cv_ptr->image;
-    cv::imshow(OPENCV_WINDOW, view);
-
-}
-
 
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "depth_processing");
-    //ImageConverter ic;
-    cv::namedWindow(OPENCV_WINDOW);
-    ros::NodeHandle nh;
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("/realsense/camera/depth/image_raw", 2, depthCallback);
-                                                   //ros::VoidPtr(),image_transport::TransportHints("raw"));
+    ImageConverter ic;
 
     ros::spin();
-    //ros::spinOnce();
+
 
     return 0;
 }
